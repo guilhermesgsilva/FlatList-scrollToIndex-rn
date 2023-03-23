d78719898690f4536000fc972b5eb6d96bde00a0
Guilherme Silva <silva.gsg@gmail.com>
2023-03-23 10:34:49 +0000

    feat: First commit

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Text,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+const TEMP_DATA = Array.from({ length: 50 }, (v, i) => i);
 
 export default function App() {
+  const [ref, setRef] = useState(null);
+  const [choosenItem, setChoosenItem] = useState(0);
+
+  const isChoosenItem = (index) => {
+    if(index === choosenItem){
+      return true;
+    }
+    return false;
+  };
+
+  const ItemRender = ({ item, index }) => (
+    <TouchableOpacity
+      onPress={() => {
+        setChoosenItem(index);
+      }}
+      style={[isChoosenItem(index) && {backgroundColor: 'pink'},{ padding: 12 }]}
+    >
+      <Text style={ styleSheet.itemText}> Element : {item} </Text>
+    </TouchableOpacity>
+  );
+
+  const Divider = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "black",
+        }}
+      />
+    );
+  };
+
+  const Header = () => {
+    return (
+      <View style={{ padding: 12, alignItems: "center" }}>
+        <Text style={styleSheet.itemText}>List Header</Text>
+      </View>
+    );
+  };
+
+  const Footer = () => {
+    return (
+      <View style={{ padding: 12, alignItems: "center" }}>
+        <Text style={styleSheet.itemText}>List Footer</Text>
+      </View>
+    );
+  };
+
+  const NoData = () => {
+    return (
+      <View>
+        <Text> No data present!</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styleSheet.mainContainer}>
+      <View style={styleSheet.buttonView}>
+        <Button
+          title="Scroll To Choosen Item"
+          onPress={() => {
+            ref.scrollToIndex({
+              animated: true,
+              index: choosenItem,
+              viewPosition: 0,
+            });
+          }}
+        />
+      </View>
+
+      <FlatList
+        ListHeaderComponent={Header}
+        ListFooterComponent={Footer}
+        ListEmptyComponent={NoData}
+        data={TEMP_DATA}
+        ref={(ref) => {
+          setRef(ref);
+        }}
+        renderItem={(itemData) => (
+          <ItemRender index={itemData.index} item={itemData.item} />
+        )}
+        ItemSeparatorComponent={Divider}
+        keyExtractor={(itemData) => itemData.toString()}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styleSheet = StyleSheet.create({
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  itemText: {
+    fontSize: 25,
+    color: "black",
+  },
+
+  buttonView: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "#2aff00",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
